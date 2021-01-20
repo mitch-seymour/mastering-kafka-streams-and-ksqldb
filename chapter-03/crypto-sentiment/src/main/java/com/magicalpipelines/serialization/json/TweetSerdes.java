91abf775413498993537c8bcf589d3f81ed5fa62
Mitch Seymour <mitchseymour@gmail.com>
2021-01-20 10:59:31 -0500

    Add Override annotations

--- chapter-03/crypto-sentiment/src/main/java/com/magicalpipelines/serialization/json/TweetSerdes.java
+++ chapter-03/crypto-sentiment/src/main/java/com/magicalpipelines/serialization/json/TweetSerdes.java
@@ -7,10 +7,12 @@ import org.apache.kafka.common.serialization.Serializer;
 
 public class TweetSerdes implements Serde<Tweet> {
 
+  @Override
   public Serializer<Tweet> serializer() {
     return new TweetSerializer();
   }
 
+  @Override
   public Deserializer<Tweet> deserializer() {
     return new TweetDeserializer();
   }
